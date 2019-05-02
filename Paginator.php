f7e82a85351ee57cc4b2758b1ff9f2e440b457a1
Emisyl <john@emisyl.com>
2019-05-02 21:29:01 +0800

    updated

--- Paginator.php
+++ Paginator.php
@@ -47,11 +47,11 @@ class Paginator
         //if not a valid number then set to default 30
         $limit = (int)$limit > 0 ? $limit : 30;
 
-        $this->limit = $limit;
-        $this->page = $page;
-
         //if not a valid number then set to 1
         $page = (int)$page > 0 ? $page : 1;
+
+        $this->limit = $limit;
+        $this->page = $page;
 
         //get the offset
         $offset = ($page - 1) * $this->limit;
@@ -81,7 +81,7 @@ class Paginator
         $pages = ceil($results->num_rows / $this->limit);
 
         //return empty string is not data from the query
-        if ($results->num_rows == 0)
+        if (count($results) <= $this->limit)
             return '';
 
         $prevLI = '';
@@ -117,37 +117,45 @@ class Paginator
         //half of number to show then round off the number
         $halfOfNoToShow = round($noToShow / 2);
 
-        //if the current page is less than or equal to the half of the number to show plus 1 then
-        //set start = 1 and end = number to show which is 10 set above
-        if ($currentPage <= $halfOfNoToShow + 1) {
-            $start = 1;
-            $end = $noToShow;
-        } else {
-            //set the start to the current page (minus) half of the number to show plus 1
-            $start = ($currentPage - $halfOfNoToShow) + 1;
+        if ($pages > $noToShow) {
+            //if the current page is less than or equal to the half of the number to show plus 1 then
+            //set start = 1 and end = number to show which is 10 set above
+            if ($currentPage <= $halfOfNoToShow + 1) {
+                $start = 1;
+                $end = $noToShow;
+            } else {
+                //set the start to the current page (minus) half of the number to show plus 1
+                $start = ($currentPage - $halfOfNoToShow) + 1;
 
-            //set the end to the current page (plus) the number to show (minus) half of the number to show plus 1
-            $end = $currentPage + ($noToShow - ($halfOfNoToShow + 1));
+                //set the end to the current page (plus) the number to show (minus) half of the number to show plus 1
+                $end = $currentPage + ($noToShow - ($halfOfNoToShow + 1));
 
-            //override the start an end
-            //if the result of end is greater of equal to the total pages
-            if ($end >= $pages) {
+                //override the start an end
+                //if the result of end is greater of equal to the total pages
+                if ($end >= $pages) {
 
-                //then set the start to the pages (minus) the number to show then (plus) 1
-                $start = ($pages - $noToShow) + 1;
+                    //then set the start to the pages (minus) the number to show then (plus) 1
+                    $start = ($pages - $noToShow) + 1;
 
-                //set end equal to pages, in this example total pages is (equal) to 20 (minus) 1
-                //(minus) 1 so end will always be number of pages minus 1
-                //so if 20 then always 19 here would be the last page
-                //because the last 20 is populated below after the for loop
-                $end = $pages - 1;
+                    //set end equal to pages, in this example total pages is (equal) to 20 (minus) 1
+                    //(minus) 1 so end will always be number of pages minus 1
+                    //so if 20 then always 19 here would be the last page
+                    //because the last 20 is populated below after the for loop
+                    $end = $pages - 1;
+                }
             }
+        }  else {
+            $start = 1;
+            $end = $pages;
         }
 
-        //adding the ellipses and first page number 1
-        if ($currentPage >= ($noToShow - $start)) {
-            $links .= '<li style="list-style:none;display: inline;"><a href="?page=1">1</a></li>';
-            $links .= '<li style="list-style:none;display: inline;">...</li>';
+
+        if ($pages > $noToShow) {
+            //adding the ellipses and first page number 1
+            if ($currentPage >= ($noToShow - $start)) {
+                $links .= '<li style="list-style:none;display: inline;"><a href="?page=1">1</a></li>';
+                $links .= '<li style="list-style:none;display: inline;">...</li>';
+            }
         }
 
         for ($no = $start; $no <= $end; $no++) {
@@ -156,15 +164,17 @@ class Paginator
         }
 
 
-        //adding the ellipses and last page number
-        if ($end < $pages - 1) {
-            $links .= '<li style="list-style:none;display: inline;">...</li>';
-            $links .= '<li style="list-style:none;display: inline;"><a href="?page=' . $pages . '">'.$pages.'</a></li>';
-        } else {
+        if ($pages > $noToShow) {
+            //adding the ellipses and last page number
+            if ($end < $pages - 1) {
+                $links .= '<li style="list-style:none;display: inline;">...</li>';
+                $links .= '<li style="list-style:none;display: inline;"><a href="?page=' . $pages . '">' . $pages . '</a></li>';
+            } else {
 
-            //append the last number
-            $noLabel = $currentPage == $pages ? '<b>[' . $pages . ']</b>' : $pages;
-            $links .= '<li style="list-style:none;display: inline;"><a href="?page=' . $pages . '">'.$noLabel.'</a></li>';
+                //append the last number
+                $noLabel = $currentPage == $pages ? '<b>[' . $pages . ']</b>' : $pages;
+                $links .= '<li style="list-style:none;display: inline;"><a href="?page=' . $pages . '">' . $noLabel . '</a></li>';
+            }
         }
 
         $links .= $nextLI;
